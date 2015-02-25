c012dc7b36d44dd6ae6fd6646eac34fbe99f6583
Andrew Clinton <ddrew.clinton@gmail.com>
2015-02-24 21:56:41 -0500

    going to current day works

--- index.js
+++ index.js
@@ -1,26 +1,42 @@
 var id_list = [];
+var toggle = true;
+
+/**
+ *	This method creates an object list of all the ids
+ *	of each row in the schedule table.
+ */
 function getIds() {
-	var table = document.getElementById('schedule');
-	for(var i = 0, row; row = table.rows[i]; i++){
-		if(i == 0) continue;
-		var id = row.id;
-		var date = new Date(id);
-		var object = {
-			'hash': id
-		}
-		id_list[date.getTime()] = object;
-	}
+    var table = document.getElementById('schedule');
+    for (var i = 0, row; row = table.rows[i]; i++) {
+        if (i == 0) continue;
+        var id = row.id;
+        var date = new Date(id);
+        var object = {
+            'hash': id,
+            'time': date.getTime()
+        }
+        id_list.push(object);
+    }
 }
 
+/**
+ *	This method searches through the {@code id_list} and searches for a
+ *	time in the {@code id_list} that is {@code >= current && <= next},
+ *	and then changes the window location hash to the id of that time.
+ */
 function goToDay() {
-	window.location.hash = "";
-	var today = (new Date()).getTime();
-	for(var time in id_list){
-		if(today >= time){
-			console.log('today: ' + (new Date(today)) + ' found: ' + (new Date(time));
-			window.location.hash = id_list[time].hash;
-			break;
-		}
-	}
-	return false;
+    window.location.hash = "";
+    var today = (new Date()).getTime();
+    for (var i = 0; i < id_list.length; i++) {
+        var current = id_list[i];
+        var next = id_list[i + 1];
+        if (today >= current.time && today <= next.time) {
+            location.hash = current.hash;
+            break;
+        }
+    }
+}
+
+function toggleTable() {
+    
 }
